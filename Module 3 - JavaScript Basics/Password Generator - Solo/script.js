b9fadd4f4a10443586dd69e77aacf99393b18dd3
HopeRandom <HopeRandom@users.noreply.github.com>
2024-12-10 17:46:03 +0000

    initial commit
    JavaScript challenges part 2 - solo project

---
+++ Module 3 - JavaScript Basics/Password Generator - Solo/script.js
@@ -0,0 +1,26 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
+
+const generateBtn = document.querySelector('.generate-btn')
+const passwordOne = document.getElementById("passwordOne")
+const passwordTwo = document.getElementById("passwordTwo")
+let passwordLength = 15
+
+
+generateBtn.addEventListener('click', function() {
+    passwordOne.textContent = createPassword()
+    passwordTwo.textContent = createPassword()
+})
+
+function getRandomCharacter() {
+    let randomCharacter = Math.floor(Math.random() * characters.length)
+    return characters[randomCharacter]
+}
+
+function createPassword() {
+    let password = ""
+    for (let i = 0; i < passwordLength; i++) {
+        password += getRandomCharacter()
+    }
+    return password
+}
