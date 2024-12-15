198c7fb07b2bc65822d5f8f9b5cc0df1672daba1
HopeRandom <HopeRandom@users.noreply.github.com>
2024-12-15 20:29:05 +0000

    initial commit

---
+++ Module 3 - JavaScript Basics/Basketball Scoreboard - Solo/index.js
@@ -0,0 +1,35 @@
+const homeScoreEl = document.getElementById("home-score")
+const guestScoreEl = document.getElementById("guest-score")
+let homeScore = 0
+let guestScore  = 0
+
+function increaseHomeScoreOne() {
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
+}
+
+function increaseHomeScoreTwo() {
+    homeScore += 2
+    homeScoreEl.textContent = homeScore
+}
+
+function increaseHomeScoreThree() {
+    homeScore += 3
+    homeScoreEl.textContent = homeScore
+}
+
+
+function increaseGuestScoreOne() {
+    guestScore += 1
+    guestScoreEl.textContent = guestScore
+}
+
+function increaseGuestScoreTwo() {
+    guestScore += 2
+    guestScoreEl.textContent = guestScore
+}
+
+function increaseGuestScoreThree() {
+    guestScore += 3
+    guestScoreEl.textContent = guestScore
+}
