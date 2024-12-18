a608a5b0eb00b30ba509bc9370bc826e0e2f3991
HopeRandom <HopeRandom@users.noreply.github.com>
2024-12-18 22:03:09 +0000

    stretch goal - highlight the leader

--- Module 3 - JavaScript Basics/Basketball Scoreboard - Solo/index.js
+++ Module 3 - JavaScript Basics/Basketball Scoreboard - Solo/index.js
@@ -7,32 +7,38 @@ let guestScore  = 0
 function increaseHomeScoreOne() {
     homeScore += 1
     homeScoreEl.textContent = homeScore
+    highlightWinner()
 }
 
 function increaseHomeScoreTwo() {
     homeScore += 2
     homeScoreEl.textContent = homeScore
+    highlightWinner()
 }
 
 function increaseHomeScoreThree() {
     homeScore += 3
     homeScoreEl.textContent = homeScore
+    highlightWinner()
 }
 
 
 function increaseGuestScoreOne() {
     guestScore += 1
     guestScoreEl.textContent = guestScore
+    highlightWinner()
 }
 
 function increaseGuestScoreTwo() {
     guestScore += 2
     guestScoreEl.textContent = guestScore
+    highlightWinner()
 }
 
 function increaseGuestScoreThree() {
     guestScore += 3
     guestScoreEl.textContent = guestScore
+    highlightWinner()
 }
 
 resetEl.addEventListener('click', () => {
@@ -40,4 +46,20 @@ resetEl.addEventListener('click', () => {
     guestScore = 0
     homeScoreEl.textContent = homeScore
     guestScoreEl.textContent = guestScore
+
+    guestScoreEl.classList.remove("highlight")
+    homeScoreEl.classList.remove("highlight")
 })
+
+function highlightWinner() {
+    if (homeScore > guestScore) {
+        homeScoreEl.classList.add("highlight")
+        guestScoreEl.classList.remove("highlight")
+    } else if (guestScore > homeScore) {
+        guestScoreEl.classList.add("highlight")
+        homeScoreEl.classList.remove("highlight")
+    } else {
+        guestScoreEl.classList.add("highlight")
+        homeScoreEl.classList.add("highlight")
+    }
+}
